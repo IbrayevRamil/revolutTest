ea84de62106880992556258b59554d4cdc7bdb2a
Ramil <ramil1dan@gmail.com>
2019-02-12 23:41:22 +0300

    minor fix

--- src/main/java/com/revolut/task/rest/controller/AccountController.java
+++ src/main/java/com/revolut/task/rest/controller/AccountController.java
@@ -35,7 +35,6 @@ public class AccountController {
                 return Messages.NO_EXISTING_ACCOUNT.getMsg();
             }
             return gson.toJson(account);
-
         });
 
         /*
